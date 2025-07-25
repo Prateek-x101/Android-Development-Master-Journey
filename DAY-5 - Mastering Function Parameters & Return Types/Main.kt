58abefdfd34832a703e177bfe871fac23c7c2a06
Prateek-x101 <154602686+Prateek-x101@users.noreply.github.com>
2025-07-25 19:08:02 +0530

    Day 5 - Mastering functions and Return types

--- DAY-5 - Mastering Function Parameters & Return Types/Main.kt
+++ DAY-5 - Mastering Function Parameters & Return Types/Main.kt
@@ -10,7 +10,7 @@ fun(to define a function) sayhello (to name the function) () -> (to define the f
 { (to define the function body)
     println("Hello, Kotlin World!")
 }
-
+ 
 function name : can be anything
 parameters : can be the datatypes of the parameters 
 mutliple paramters : separated by coma , .
